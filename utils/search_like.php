917757859131625c78a017bfec899c6d112dd8e1
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-26 15:19:11 -0300

    Operador Like

---
+++ utils/search_like.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$conn = require __DIR__.'/../conection.php';
+
+$term = $argv[1] ?? null;
+
+$stmt = $conn->prepare('SELECT * from posts WHERE title LIKE ?');
+
+$stmt->bind_param('s',$term);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+
+$posts = $result->fetch_all(MYSQLI_ASSOC);
+
+
+foreach ($posts as $post)
+{
+    echo $post['title'].PHP_EOL;
+    echo $post['body'].PHP_EOL;
+    echo PHP_EOL;
+}
