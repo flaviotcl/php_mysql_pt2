4a60f65630eb2dae251469268fe998cf7d0d280a
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-27 11:20:09 -0300

    Manipulando erros

--- conection.php
+++ conection.php
@@ -1,5 +1,23 @@
 <?php
 
+$debug = TRUE;
+
+/***
+ *   MYSQL_REPORT_ERROR
+ *   MYSQL_REPORT_OFF
+ *   MYSQL_REPORT_STRICT   --> Retorna em formato de Exception.
+ *   MYSQL_REPORT_INDEX    --> Verifica se existe algum índice quebrado. 
+ *   MYSQL_REPORT_ALL  
+ ***/
+
+if($debug)
+{
+    mysqli_report(MYSQLI_REPORT_ERROR);    
+}    
+else
+{
+    mysqli_report(MYSQLI_REPORT_OFF);
+}
 $conn = new mysqli('localhost','root','123456','php_mysql_avancando');
 
 if($conn->connect_errno)
