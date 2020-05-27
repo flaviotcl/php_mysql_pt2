bffc6652e4ea2b6426ec7941e3cf2fc8ee1145c3
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-27 11:02:15 -0300

    Transaction

--- utils/create_post_table.php
+++ utils/create_post_table.php
@@ -3,12 +3,29 @@
 
 $conn = require __DIR__.'/../conection.php';
 
+!$conn->query('DROP TABLE posts');
 
+/***
+ *  O $sql abaixo foi utilizado p/ exemplo busca por Relevância, 
+ *  foi setada a ENGINE = MyISAM.
+ */
+
+/***
+  $sql ='
+        CREATE TABLE posts(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(50) NOT NULL,
+            body TEXT NOT NULL,
+            FULLTEXT key title(title,body)
+        ) ENGINE = MyISAM;
+    ';
+***/
 $sql ='
         CREATE TABLE posts(
             id INT AUTO_INCREMENT PRIMARY KEY,
             title VARCHAR(50) NOT NULL,
-            body TEXT NOT NULL
+            body TEXT NOT NULL,
+            FULLTEXT key title(title,body)
         )
     ';
 
--- utils/insert_posts.php
+++ utils/insert_posts.php
@@ -3,12 +3,25 @@
 
 $conn = require __DIR__.'/../conection.php';
 
+$save = true;
+
 $conn->query('TRUNCATE posts');
 
 $sql= file_get_contents(__DIR__.'/insert_posts.sql');
 
+$conn->begin_transaction();
+
 $conn->query($sql);
 
+if($save)
+{
+    $conn->commit();    
+}else
+{
+    $conn->rollback();
+}
+
+echo  "START SELECT ".PHP_EOL; 
 
 $rs = $conn->query('SELECT * FROM posts');
 
@@ -22,3 +35,5 @@ foreach ($posts as $post)
     echo $post['body'].PHP_EOL;
     echo PHP_EOL;
 }
+
+echo  "END SELECT ".PHP_EOL;
